542bfd1e3823bf1e895df373e417a0e0408a89b3
DanaBM-405451 <405451@tecnicatura.frc.utn.edu.ar>
2025-05-21 15:37:02 -0300

    Segundo ejercicio :)

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -11,11 +11,26 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
 
-        System.out.println(a);
+
+
+            System.out.println("Ingrese 3 valores");
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+
+        Scanner scanner = new Scanner(System.in);
+        String myString = scanner.next();
+        int myInt = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("myString is: " + myString);
+        System.out.println("myInt is: " + myInt);
         // Complete this line
         // Complete this line
 
